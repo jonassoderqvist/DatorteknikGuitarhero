c4225e1a33fcda780a3f6c0ab72a7cc01092a7c0
JonasSoderqvist <hippiehappybear@gmail.com>
2017-02-19 13:05:25 +0100

    agfd

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <pic32mx.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 extern void _enable_interrupt();
 
@@ -8,9 +9,20 @@ void setPwm(int pwm, int duty){
 	OC1RS = duty;
 	PR2 = pwm;
 }
-
-void initSynth() {
+initSong(int song){
+    if(song==1){
+        return {1,3,0,5,0,3,1,4,2,1,3,0,5,0,3,1,4,2,1,3,0,5,0,3,1,4,2};
+}
+void runGame(int song[50], int speed) {
+    int score = 0;
+    x=0;
 	for(;;) {
+        int note = song[x];
+        if(i==6000/speed){
+            x++;
+            i=0;
+        }
+        i++;
 		int btns = getBtns();
 		// Check buttons. If button is pressed, corresponding note's play-value will be set to 1 (true)
 		if((PORTD & 0b000000100000) == 0b000000100000){
@@ -43,7 +55,7 @@ void initPwm(){
 	OC1R = 0x0001;
 	OC1RS = 0; // Dutycycle
 	OC1CON = 0x0006; // Configure for PWM mode without Fault pin
-	PR2 = 0; // Set dutycycle, HÄR ÄNDRAR MAN TONER!
+	PR2 = 0;
 	IECSET(0) = 0x0100; // Enable T2 interrupt
 	IPCSET(2) = 0x01C; // Set T2 interrupt priority to 7
 	enable_interrupt();
@@ -51,12 +63,42 @@ void initPwm(){
 	// Om något knasar, titta lite djupare på OC1CONSET!!!
 	T2CONSET |= 0x08000; // Enable Timer2
 }
-
+    int speed(){
+        for(;;){
+            if((PORTD & 0b000000100000) == 0b000000100000){
+                return 1;
+            }
+            if((PORTD & 0b000001000000) == 0b000001000000){
+                return 2;
+            }
+            if((PORTD & 0b000010000000) == 0b000010000000){
+                return 3;
+            }
+        }
+    }
+    int menu(){
+        for(;;){
+        if((PORTD & 0b000000100000) == 0b000000100000){
+            return 1;
+        }
+        if((PORTD & 0b000001000000) == 0b000001000000){
+            return 2;
+        }
+        if((PORTD & 0b000010000000) == 0b000010000000){
+            return 3;
+        }
+    }
+    }
 int main(void) {
+    srand(time(NULL));
 	TRISE = 0x00; 	/* Port E bits 0 through 7 is used for the LED and is set to 0 (output) */
 	PORTE = 0x00;
+    int song = menu();
+    int speed = speed();
 	initPwm();
-	initSynth();
+    int songArr[] = initSong(song);
+	runGame(songArr, speed);
+    showScore();
 	return 0;
 }
 
